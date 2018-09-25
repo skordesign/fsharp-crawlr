bdaad2ebfa224c748f5feaecc371a9e2535d47ff
Huynh tuan Phat <skordesign@outlook.com>
2018-09-25 21:42:32 +0700

    update

--- Program.fs
+++ Program.fs
@@ -2,44 +2,60 @@
 open canopy.runner.classic
 open canopy.configuration
 open canopy.classic
+open canopy
 open OpenQA.Selenium.Chrome
-open System.Threading.Tasks;
-open System.Threading
-open System.Collections.ObjectModel
+open OpenQA.Selenium.Support.UI
+open System
+open OpenQA.Selenium
 
 //start an instance of chrome
-chromeDir <- "./"
+chromeDir <- "."
 
-start chrome
+let chromeOptions = OpenQA.Selenium.Chrome.ChromeOptions()
+chromeOptions.AddUserProfilePreference ("download.default_directory", @"C:\Workspace\Downloads")
+
+
+let driver = new ChromeDriver(".",chromeOptions)
+
+
+let downloadButtonInPanel = """button[data-test-selector="download-without-license"]"""
+
+let downloadDisplayed e =
+    (driver.FindElementByCssSelector e).Displayed
+
+let downloadDisapear e =
+    (driver.FindElementByCssSelector e) = null
+
+let download (e : IWebElement) =
+    e.Click() |> ignore
+    let button = driver.FindElementByCssSelector(downloadButtonInPanel)
+    button.Click() |> ignore
+    let btn2 = driver.FindElementByCssSelector(downloadButtonInPanel)
+    while btn2 = null do
+       sleep 1
 
 let ctrlClickAll selector =
-    elements selector
-    |> List.iter (fun element -> ctrlClick element)
+    driver.FindElementsByCssSelector selector
+    |> Seq.iter (fun element -> 
+    download element
+    sleep 3
+    )
+    
 
-let seq (readonlyCollection: ReadOnlyCollection<string>) = 
-    Seq.cast readonlyCollection |> Seq.mapi (fun f -> fun v -> f)
 //this is how you define a test
-"taking canopy for a spin" &&& fun _ ->
+"Travel to envato" &&& fun _ ->
     //this is an F# function body, it's whitespace enforced
 
     //go to url
-    url "https://elements.envato.com/sign-in"
+    driver.Navigate().GoToUrl "https://elements.envato.com/sign-in"
 
-    while currentUrl () = "https://elements.envato.com/sign-in" do
-        Thread.Sleep(3000)
+    while driver.Url = "https://elements.envato.com/sign-in" do
+        sleep 3
     
-    url "https://elements.envato.com/stock-video"
+    driver.Url <- "https://elements.envato.com/stock-video"
 
-    ctrlClickAll """a[class="_1wL0Kx7A"]"""
+    ctrlClickAll """button[data-test-selector="item-card-download-button"]"""
     
-    seq browser.WindowHandles |> Seq.iter (fun i ->
-        i+2 |> printfn "%d"
-            |> ignore
-        i+2 |> switchToTab
-            |> ignore
-
-        Thread.Sleep(3000)
-    )
 run()
 
 printfn "press [enter] to exit"

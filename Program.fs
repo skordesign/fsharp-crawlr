a5a792e3d5dc5167e0c366b78edbc14e51aef88f
Hi I'm Bugger <skordesign@outlook.com>
2018-09-25 17:18:15 +0700

    update

--- Program.fs
+++ Program.fs
@@ -1,8 +1,48 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿//these are similar to C# using statements
+open canopy.runner.classic
+open canopy.configuration
+open canopy.classic
+open OpenQA.Selenium.Chrome
+open System.Threading.Tasks;
+open System.Threading
+open System.Collections.ObjectModel
 
-open System
+//start an instance of chrome
+chromeDir <- "./"
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+start chrome
+
+let ctrlClickAll selector =
+    elements selector
+    |> List.iter (fun element -> ctrlClick element)
+
+let seq (readonlyCollection: ReadOnlyCollection<string>) = 
+    Seq.cast readonlyCollection |> Seq.mapi (fun f -> fun v -> f)
+//this is how you define a test
+"taking canopy for a spin" &&& fun _ ->
+    //this is an F# function body, it's whitespace enforced
+
+    //go to url
+    url "https://elements.envato.com/sign-in"
+
+    while currentUrl () = "https://elements.envato.com/sign-in" do
+        Thread.Sleep(3000)
+    
+    url "https://elements.envato.com/stock-video"
+
+    ctrlClickAll """a[class="_1wL0Kx7A"]"""
+    
+    seq browser.WindowHandles |> Seq.iter (fun i ->
+        i+2 |> printfn "%d"
+            |> ignore
+        i+2 |> switchToTab
+            |> ignore
+
+        Thread.Sleep(3000)
+    )
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
